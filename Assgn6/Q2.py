dfa7e4ba6916d30e7dc02059646b6ee22e340f5d
Monu Kumar <82472152+Enigma811@users.noreply.github.com>
2022-11-25 13:45:37 +0530

    Revision plotting - Q2

--- Assgn6/Q2.py
+++ Assgn6/Q2.py
@@ -4,13 +4,15 @@ from mypylib import random as rn
 
 from matplotlib import pyplot as plt
 
-def bdODE(f1,f2,x0,y10,y11,y20,tol,h=0.1,n=50):
+def bdODE(f1,f2,x0,y10,y11,y20,tol,con=0,h=0.1,n=100):
 # 'f1' - (dy1/dx) 
 # 'f2' - (dy2/dx) or (d^2y1/dx^2)
 # 'x0' - independent variable - initial value
 # 'y10' - y1 - initial value
 # 'y11' - y1 - final value
 # 'y20' - y2 or dy1/dx - initial value; "a" is placeholder - random initial guess in range 0,100
+# 'tol' - tolerance
+# 'con' - check to plot convergence; default - 0 = False, 1 = True
 # 'h' - step size of independent variable
 # 'n' - max iteration count
     
@@ -28,6 +30,8 @@ def bdODE(f1,f2,x0,y10,y11,y20,tol,h=0.1,n=50):
     ogh = 0 # high(overestimating) initial guess for y2
     y11l = 0 # corresponding low boundary value of y1
     y11h = 0 # corresponding high boundary value of y1
+    CG = []
+
     for j in range(n):
         x = x0
         xn = x0
@@ -37,6 +41,7 @@ def bdODE(f1,f2,x0,y10,y11,y20,tol,h=0.1,n=50):
         res = [[x0,y10]]
         if j==0: cg = y2
         y2n = cg
+        CG.append(cg)
 
         for i in range(1,n):
             x = x0 + (i-1)*h
@@ -63,7 +68,20 @@ def bdODE(f1,f2,x0,y10,y11,y20,tol,h=0.1,n=50):
             # res[i] = res[i-1] + (h/6)*(t1 + 4*t2 + t3)
             res[(len(res)-1)].append(x0 + i*h)
             res[(len(res)-1)].append(y1n)
+        
+        s=20
+        p = []
+        q = []
+        for i in range(len(res)):
+            p.append(res[i][0])
+            q.append(res[i][1])
 
+        plt.scatter(p,q, marker='.', s=s)
+        plt.grid()
+        plt.legend(CG,loc='lower right')
+        plt.xlabel("Position (x) (m)")
+        plt.ylabel("Temperature (T(x) (deg C))")
+        plt.title("Revision of estimated sol with intital guess for dT/dx")
 
         if (abs(res[len(res)-1][1] - y11) > tol) & (res[len(res)-1][1] < y11):
             if (ck_l == 1) & (res[len(res)-1][1]>y11l): 
@@ -119,6 +137,10 @@ def bdODE(f1,f2,x0,y10,y11,y20,tol,h=0.1,n=50):
                 y2=cg
             
         elif abs(res[len(res)-1][1] - y11) < tol:
+            if con == 1: plt.savefig("Q2 Convergence.png")
+            del p
+            del q
+            del CG
             if y20 == "a": b="Random"
             else: b = y20
             return res,j,b
@@ -126,6 +148,9 @@ def bdODE(f1,f2,x0,y10,y11,y20,tol,h=0.1,n=50):
     if j == n-1: 
         print("Error: Max Iteration count exceeded")
         print("Error Handling: Increase Max Iteration count or imput another inital guess for dy/dx")
+        del p
+        del q
+        del CG
         return
 
 # may give functions as input from file
@@ -133,7 +158,7 @@ fn = "y2"
 gn = "0.01*(y1 - 20)"
 s=20
 
-pr,iter,ini = bdODE(fn,gn,0,40,200,"a",0.0001,0.1,100)
+pr,iter,ini = bdODE(fn,gn,0,40,200,"a",0.0001)
 
 #plotting
 p = []
@@ -177,6 +202,6 @@ fout.writelines("T(0) = 40, T(10) = 200\n\n")
 fout.writelines("Assume that the left end of the rod is kept at origin.\n")
 fout.writelines("Length of rod = 10m\n\n")
 fout.writelines("Initial guess for dT/dx: "+str(ini)+"\n\n")
-fout.writelines("Number of iterations to achieve given precision: "+str(iter)+"\n\n")
-fout.writelines("Estimated position at which T(x) = 100: "+str(pos)+" m\n")
+fout.writelines("Number of revisions to achieve given precision: "+str(iter)+"\n\n")
+fout.writelines("Estimated position at which T(x) = 100: "+str(float(f'{pos:.5f}'))+" m\n")
 
